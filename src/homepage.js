e8420d8ede95f63ba37a725d2676d84319d0f7bf
Danisha2002sahu <danishasahu86@gmail.com>
2024-06-23 01:29:36 +0530

    Homepage Added

--- src/homepage.js
+++ src/homepage.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './homepage.css';
+import topsectionimage from './topsectionimage1.jpg';
+import img1 from './1.jpg'
+import img2 from './2.jpg'
+import img3 from './3.jpg'
 
     function Navbar(){
         return(
@@ -14,15 +18,34 @@ import './homepage.css';
     
         )
       }
+       
+    function HomepageTopSection(){
+        return(
+            //write the html code here
+           <div className="HomepageTopSection">
+<img src={topsectionimage} alt="topimage"></img>
+           </div>
+
+        )
+    }
+    
+    function ProductCarousel(){
+        return(
+            <div classname="ProductCarousel">
+                <img src={img1} alt="kuchbhi"></img>
+                <img src={img2} alt="fhf"></img>
+                <img src={img3} alt="akhi"></img>
+            </div>
+        )
+    }
 
 
-// As we are supposed to show all the components of homepage.js as a whole page on the website
-// Insted of exporting and importing all the components indivisually we will wrap it in a parentcomponent and export the parentcomponent
-// As wholePage.
       function TotalhomePage(){
         return(
             <div>
             <Navbar/>
+            <HomepageTopSection/>
+            <ProductCarousel/>
             </div>
         )
       }
